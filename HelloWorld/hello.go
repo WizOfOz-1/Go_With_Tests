a59f944670ebaf618b4b9344a54df26b184d56c6
WizOfOz-1 <pokharel.swoyam.1@gmail.com>
2024-10-17 09:36:00 +0545

    Completed Hello World with TDD

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(receipent string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(receipent, language string) string {
 	if receipent == "" {
-        receipent = "World "
+		receipent = "World"
 	}
-	return englishHelloPrefix + receipent
+	return greetingPrefix(language) + receipent
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
